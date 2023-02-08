6aee2a85f6dbca09cb0356709d62cad48a445681
Михаил Яковенко <yakovenki@yandex.ru>
2023-02-08 11:52:19 +0300

    exercises for review

--- test_items.py
+++ test_items.py
@@ -4,6 +4,5 @@ from selenium.webdriver.common.by import By
 
 def test_basket_button(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    time.sleep(10)
     button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert len(button) != 0, "Button wasnt found"
